5619c9debccc2e338621a5c6997a02edd0e16d34
@chambois <mwchamberlain@gmail.com>
2017-02-28 10:56:45 +1100

    @chambois: Wait for Jekyll to finish before running specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,17 +4,15 @@ require 'rack/jekyll'
 require 'rack/test'
 
 RSpec.configure do |config|
-  # Configure Capybara to load the website through rack-jekyll.
-  # (force_build: true) builds the site before the tests are run,
-  # so our tests are always running against the latest version
-  # of our jekyll site.
 
   config.before(:all) do
-    Capybara.app = Rack::Jekyll.new(force_build: true)
-    visit '/'
+    app = Rack::Jekyll.new(force_build: true)
 
-    expect(page).not_to have_content 'Jekyll is currently rendering the site'
+    Capybara.app = app
+
+    while(app.compiling?)
+      sleep(1)
+    end
   end
 
-  #Capybara.app_host = 'http://homework-prototype.s3-website-ap-southeast-2.amazonaws.com'
 end
